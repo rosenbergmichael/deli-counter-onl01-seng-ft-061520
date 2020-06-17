9cdfeb367f4a544d2486f108ae69c95af8c6e944
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-17 22:45:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,10 @@ def line(line_array)
   if line_array.count==0
   puts "The line is currently empty."
 else 
+line_string="The line is currently:"
 line_string.each_line do |name, index|
   puts "The line is currently: #{index}. #{name}"
-  line_array << line_string
+  line_string << line_array
 end
 line_string
 end
