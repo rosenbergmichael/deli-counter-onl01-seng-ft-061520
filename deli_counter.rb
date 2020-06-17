48980ed447f22f81004fad60ffa354267d17b85f
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-17 23:06:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,7 @@ end
 def take_a_number(katz_deli, name_person)
   katz_deli.each_with_index do |name, index|
     puts "Welcome, #{name}. You are number #{index +1} in line."
+name_person="Welcome to Katz's!"
 end
 end
+
